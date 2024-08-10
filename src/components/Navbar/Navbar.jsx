8708626d266da2da44fce8aff8ea8fbd329d740f
ahadsaim9 <107304817+ahadsaim9@users.noreply.github.com>
2024-08-10 14:29:36 +0600

    added some item

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
     <section className="fixed top-0 right-0 w-full color-1 z-10 ">
       <div className="flex   relative justify-between h-16  px-5 items-center shadow-lg">
         <NavLink to="/">
-          <h1 className="text-white md:text-2xl font-bold italic  ">
+          <h1 className=" cursor-pointer text-white md:text-2xl font-bold italic  ">
             ASM STORE
           </h1>
         </NavLink>
         <ul
-          className={` absolute duration-1000  ${
+          className={` absolute duration-500  ${
             open ? "top-16 right-0" : "-right-[300px] top-16 "
-          } p-5 md:p-0 rounded-bl-md   md:static md:flex gap-6 text-[20px] text-white font-bold color-1 md bg-transparent`}
+          } p-5 md:p-0 rounded-bl-md   md:static md:flex gap-6 text-[20px] text-white font-bold color-1 cursor-pointer md bg-transparent`}
         >
           <li>
             <NavLink
@@ -92,13 +92,14 @@ const Navbar = () => {
         </ul>
 
         <button
+          type="button"
           onClick={() => setOpen((prev) => !prev)}
-          className="md:hidden  "
+          className="md:hidden cursor-pointer z-10 "
         >
           {open ? (
-            <MdClose className="text-2xl  md:hidden"></MdClose>
+            <MdClose className="text-2xl z-20 md:hidden hover:cursor-pointer text-white"></MdClose>
           ) : (
-            <MdMenu className="text-2xl md:hidden"></MdMenu>
+            <MdMenu className="text-2xl z-20 text-white hover:cursor-pointer md:hidden "></MdMenu>
           )}
         </button>
       </div>
